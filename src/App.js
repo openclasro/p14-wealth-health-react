89110e1e28b7eb81d3a6470e60e887beca9cc1f9
arezki <zaidi_arezki@ymail.com>
2022-10-31 13:16:55 +0100

    init create employee page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css"
+import CreateEmployeePage from "./pages/CreateEmployeePage"
 
 function App() {
-  return <div className="App">Hey</div>
+  return (
+    <div className="App">
+      <CreateEmployeePage />
+    </div>
+  )
 }
 
 export default App
