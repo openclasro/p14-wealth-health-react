c2be16952545d7005a338302c213edbcd030adba
arezki <zaidi_arezki@ymail.com>
2022-10-31 13:20:54 +0100

    init employees list page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css"
 import CreateEmployeePage from "./pages/CreateEmployeePage"
+import EmployeeListPage from "./pages/EmployeeListPage"
 
 function App() {
   return (
     <div className="App">
-      <CreateEmployeePage />
+      {/* <CreateEmployeePage /> */}
+      <EmployeeListPage />
     </div>
   )
 }
---
+++ src/pages/EmployeeListPage.js
@@ -0,0 +1,9 @@
+export default function EmployeeListPage() {
+  return (
+    <div id="employee-div" className="container">
+      <h1>Current Employees</h1>
+      <table id="employee-table" className="display"></table>
+      <a href="index.html">Home</a>
+    </div>
+  )
+}
