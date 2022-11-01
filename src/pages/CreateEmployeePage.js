56edc28933782eb3dcc6bac618d9ab59fafbaba7
arezki <zaidi_arezki@ymail.com>
2022-11-01 16:25:05 +0100

    create employee: init

--- src/pages/CreateEmployeePage.js
+++ src/pages/CreateEmployeePage.js
@@ -1,6 +1,27 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
 
 export default function CreateEmployeePage() {
+  const [firstName, setFirstName] = useState("")
+  const [lastName, setLastName] = useState("")
+  const [birthDate, setBirthDate] = useState("")
+  const [startDate, setStartDate] = useState("")
+  const [street, setStreet] = useState("")
+  const [city, setCity] = useState("")
+  const [state, setState] = useState("")
+  const [zipCode, setZipCode] = useState("")
+  const [department, setDepartment] = useState("Sales")
+  console.log({
+    firstName,
+    lastName,
+    birthDate,
+    startDate,
+    street,
+    city,
+    state,
+    zipCode,
+    department,
+  })
   return (
     <div>
       <div className="title">
@@ -11,35 +32,80 @@ export default function CreateEmployeePage() {
         <h2>Create Employee</h2>
         <form action="#" id="create-employee">
           <label htmlFor="first-name">First Name</label>
-          <input type="text" id="first-name" />
+          <input
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            type="text"
+            id="first-name"
+          />
 
           <label htmlFor="last-name">Last Name</label>
-          <input type="text" id="last-name" />
+          <input
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            type="text"
+            id="last-name"
+          />
 
           <label htmlFor="date-of-birth">Date of Birth</label>
-          <input id="date-of-birth" type="text" />
+          <input
+            value={birthDate}
+            onChange={(e) => setBirthDate(e.target.value)}
+            id="date-of-birth"
+            type="text"
+          />
 
           <label htmlFor="start-date">Start Date</label>
-          <input id="start-date" type="text" />
+          <input
+            value={startDate}
+            onChange={(e) => setStartDate(e.target.value)}
+            id="start-date"
+            type="text"
+          />
 
           <fieldset className="address">
             <legend>Address</legend>
 
             <label htmlFor="street">Street</label>
-            <input id="street" type="text" />
+            <input
+              value={street}
+              onChange={(e) => setStreet(e.target.value)}
+              id="street"
+              type="text"
+            />
 
             <label htmlFor="city">City</label>
-            <input id="city" type="text" />
+            <input
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+              id="city"
+              type="text"
+            />
 
             <label htmlFor="state">State</label>
-            <select name="state" id="state"></select>
+            <select
+              value={state}
+              onChange={(e) => setState(e.target.value)}
+              name="state"
+              id="state"
+            ></select>
 
             <label htmlFor="zip-code">Zip Code</label>
-            <input id="zip-code" type="number" />
+            <input
+              value={zipCode}
+              onChange={(e) => setZipCode(e.target.value)}
+              id="zip-code"
+              type="number"
+            />
           </fieldset>
 
           <label htmlFor="department">Department</label>
-          <select name="department" id="department">
+          <select
+            value={department}
+            onChange={(e) => setDepartment(e.target.value)}
+            name="department"
+            id="department"
+          >
             <option>Sales</option>
             <option>Marketing</option>
             <option>Engineering</option>
