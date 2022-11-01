b45a77ff0544f949ea4612d4f3ded85935d964eb
arezki <zaidi_arezki@ymail.com>
2022-11-01 16:55:07 +0100

    add label state

--- src/components/StatesSelector.js
+++ src/components/StatesSelector.js
@@ -1,16 +1,19 @@
 export default function StateSelector({ state, states, onSelect }) {
   return (
-    <select
-      value={state}
-      onChange={(e) => onSelect(e.target.value)}
-      name="state"
-      id="state"
-    >
-      {states.map((item) => (
-        <option key={item.abbreviation} value={item.abbreviation}>
-          {item.name}
-        </option>
-      ))}
-    </select>
+    <>
+      <label htmlFor="state">State</label>
+      <select
+        value={state}
+        onChange={(e) => onSelect(e.target.value)}
+        name="state"
+        id="state"
+      >
+        {states.map((item) => (
+          <option key={item.abbreviation} value={item.abbreviation}>
+            {item.name}
+          </option>
+        ))}
+      </select>
+    </>
   )
 }
